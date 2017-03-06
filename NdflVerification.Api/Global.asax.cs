dccb11fec74da7aaadbc3183b95de9add450d790
Loschilin <gloschilin@gmail.com>
2017-03-06 18:09:11 +0300

    Api

--- NdflVerification.Api/Global.asax.cs
+++ NdflVerification.Api/Global.asax.cs
@@ -13,6 +13,7 @@ namespace NdflVerification.Api
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            UnityConfig.RegisterComponents();
         }
     }
 }
